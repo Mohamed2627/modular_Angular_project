0dfaf630fbdd044ef7e2784f2e6448842e96d3d5
hebamsamy <hebasamy655@gmail.com>
2022-10-12 17:54:12 +0200

    fade Animation on router test it two links in nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,15 @@ import { trigger, query, transition, style, animate, animateChild, group } from 
   animations: [
     trigger("routerFade", [
       transition('* <=> *', [
-        query(':enter,:leave',
+        query(':enter',
           [style({ opacity: 0 })],
-          { optional: true }
-        ),
-        query(':leave',
-          [style({ opacity: 1 }),
-          animate('1s', style({ opacity: 0 }))],
           { optional: true }
         ),
         query(':enter',
           [style({ opacity: 0 }),
           animate('1s', style({ opacity: 1 }))],
           { optional: true }
-        ),query(':leave', animateChild(),{ optional: true }),
+        )
       ])
     ]
     ),
